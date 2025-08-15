b44b48d8b978d63e9011ee52ad07d8ce3bd56486
Julia Smirnova <iulia.smirno@gmail.com>
2025-08-15 12:29:06 +0300

    implement methods in TaskServiceImpl

--- src/main/java/ru/spbstu/taskmanager/service/impl/TaskServiceImpl.java
+++ src/main/java/ru/spbstu/taskmanager/service/impl/TaskServiceImpl.java
@@ -1,28 +1,39 @@
 package ru.spbstu.taskmanager.service.impl;
 
 import ru.spbstu.taskmanager.model.Task;
+import ru.spbstu.taskmanager.repository.TaskRepository;
 import ru.spbstu.taskmanager.service.TaskService;
 
 import java.util.List;
 
 public class TaskServiceImpl implements TaskService {
+    private final TaskRepository repository;
+
+    public TaskServiceImpl(TaskRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Task createTask(String title) {
-        return null;
+        return repository.save(new Task(title));
     }
 
     @Override
     public List<Task> getAllTasks() {
-        return List.of();
+        return repository.findAll().stream()
+                .filter(t -> !t.isDeleted())
+                .toList();
     }
 
     @Override
     public List<Task> getPendingTasks() {
-        return List.of();
+        return repository.findAll().stream()
+                .filter(t -> !t.isDeleted() && !t.isCompleted())
+                .toList();
     }
 
     @Override
     public void deleteTask(String id) {
-
+        repository.markDeleted(id);
     }
 }
